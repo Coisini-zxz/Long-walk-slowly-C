a762692eba2fef8a2eb129debd0d575c24327f70
Coisini-zxz <2501375882@qq.com>
2022-03-22 21:34:36 +0800

    default

--- 3_22/test.c
+++ 3_22/test.c
@@ -1,6 +1,6 @@
-// C语言是从主函数的第一行开始执行的
-// 1. 一个工程可以有多个.c文件 但是只能有一个main函数
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS	1	// 放在开头 防止scanf报错
+#include <stdio.h> 	
+
 int main()
 {
 	printf("打印测试\n");
@@ -14,5 +14,32 @@ int main()
 	printf("%d\n", sizeof(long long));	//更长整型
 	printf("%d\n", sizeof(float));		//单精度浮点型
 	printf("%d\n", sizeof(double));		//双精度浮点型
+	variable();
+	sum_ab();
+	return 0;
+}
+int variable()
+{
+	int age = 21;
+	double weight = 67.8;
+	age = age + 1;
+	weight = weight - 10;
+	printf("年龄 : %d\n", age);
+	printf("体重 : %lf\n", weight);
+	return 0;
+}
+// %d int
+// %f float
+// %lf double
+
+// scanf()输入函数 printf()输出函数
+int sum_ab() 
+{
+	int a = 0;
+	int b = 0;
+	int sum;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum = %d\n", sum);
 	return 0;
 }
