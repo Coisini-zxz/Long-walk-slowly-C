f849ad0c157035ac48b405d8074722322478573d
WxylkxyZz <2501375882@qq.com>
2022-05-01 17:00:40 +0800

    2022_5_1

--- C语言学习代码_高级/Section_2/Primary.c
+++ C语言学习代码_高级/Section_2/Primary.c
@@ -26,8 +26,8 @@ int main()
     // func9(); // 这到底有什么用呢？ eg 写一个计算器
     // func10(); // 由冒泡排序做引子
     // func11(); // qsrot()学习
-    func12(); // 模仿qsort函数 来实现 冒泡排序的一个通用算法
-
+    //func12(); // 模仿qsort函数 来实现 冒泡排序的一个通用算法
+    v1();
 	return 0;
 }
 
@@ -786,3 +786,230 @@ void bubble_sort_up(void* base,
         }
     }
 }
+
+
+
+//一维数组
+int v1()
+{
+    int a[] = { 1,2,3,4 };
+    printf("%d\n", sizeof(a));        // - 16
+    printf("%d\n", sizeof(a + 0));  // - 4/8  ---第一个元素的地址 计算的时地址的大小
+    printf("%d\n", sizeof(*a));       // -  4  --- 第一个元素的大小  计算的第一个元素的大小
+    printf("%d\n", sizeof(a + 1));  // -  4/8 --- 第二个元素的地址
+    printf("%d\n", sizeof(a[1]));     // - 4 --- 第二个元素的大小
+
+    printf("%d\n", sizeof(&a));       //- 4/8
+    printf("%d\n", sizeof(*&a));     //-  4 错误  取地址解引用找到的时 数组a 所以计算的数组的大小 16
+    printf("%d\n", sizeof(&a + 1));  //- ？ 数组指针+1 跳过整个数组 为4后面的地址 所以为4/8
+    printf("%d\n", sizeof(&a[0]));    // - 4/8
+    printf("%d\n", sizeof(&a[0] + 1));//  -4/8
+    return 0;
+}
+
+//字符数组
+int v2()
+{
+    char arr[] = { 'a','b','c','d','e','f' };
+    printf("%d\n", sizeof(arr));           // 6
+    printf("%d\n", sizeof(arr + 0));     // a的地址 4/8
+    printf("%d\n", sizeof(*arr));           // 1
+    printf("%d\n", sizeof(arr[1]));       // 1
+    printf("%d\n", sizeof(&arr)); // 4/8
+    printf("%d\n", sizeof(&arr + 1)); // 4/8
+    printf("%d\n", sizeof(&arr[0] + 1));  // 4/8
+
+    printf("%d\n", strlen(arr));   //  随机值
+    printf("%d\n", strlen(arr + 0)); //  随机值
+    printf("%d\n", strlen(*arr)); // 报错 传入了a a的ascll码 97  把97当作地址会出错
+    printf("%d\n", strlen(arr[1]));  // 报错 98
+    printf("%d\n", strlen(&arr)); // char(*)[6] ---> char* 随机值
+    printf("%d\n", strlen(&arr + 1)); // 随机值-6
+    printf("%d\n", strlen(&arr[0] + 1)); // 随机值-1
+}
+int v3()
+{
+    char arr[] = "abcdef";
+    printf("%d\n", sizeof(arr));         // 7
+    printf("%d\n", sizeof(arr + 0));   // 4/8
+    printf("%d\n", sizeof(*arr));   // 1
+    printf("%d\n", sizeof(arr[1]));  // 1
+    printf("%d\n", sizeof(&arr));   // 4/8
+    printf("%d\n", sizeof(&arr + 1));    // 4/8
+    printf("%d\n", sizeof(&arr[0] + 1));    // 4/8
+
+    printf("%d\n", strlen(arr)); // 6
+    printf("%d\n", strlen(arr + 0)); //6
+    printf("%d\n", strlen(*arr));    // err
+    printf("%d\n", strlen(arr[1]));  // err 
+    printf("%d\n", strlen(&arr));  // 6
+    printf("%d\n", strlen(&arr + 1));  // 随机值
+    printf("%d\n", strlen(&arr[0] + 1));  // 5
+}
+
+int v4()
+{
+    char* p = "abcdef";
+    printf("%d\n", sizeof(p));   // 4/8 
+    printf("%d\n", sizeof(p + 1));  // 4/8 
+    printf("%d\n", sizeof(*p));  // 1
+    printf("%d\n", sizeof(p[0]));  // = *(p+0)  1 
+    printf("%d\n", sizeof(&p)); // 4/8
+    printf("%d\n", sizeof(&p + 1));  // 4/8
+    printf("%d\n", sizeof(&p[0] + 1)); // 4/8 
+
+    printf("%d\n", strlen(p));   //  6
+    printf("%d\n", strlen(p + 1));    //  5
+    printf("%d\n", strlen(*p));  // 97 err
+    printf("%d\n", strlen(p[0])); // err
+    printf("%d\n", strlen(&p)); //  随机值
+    printf("%d\n", strlen(&p + 1)); // 随机值
+    printf("%d\n", strlen(&p[0] + 1));  // 5
+}
+
+//二维数组
+int v5()
+{
+    int a[3][4] = { 0 };
+    printf("%d\n", sizeof(a));                 // 48
+    printf("%d\n", sizeof(a[0][0]));         // 4
+    printf("%d\n", sizeof(a[0]));              // 16     a[0] 就等价于 第一行的数组名
+    printf("%d\n", sizeof(a[0] + 1));        // 4/8 指向了第一行第二个元素的地址 a[0]作为数组名 并没有单独放在sizeof内部 也没有取地址 所以为第一行第一个元素的地址
+    printf("%d\n", sizeof(*(a[0] + 1)));    // 4 第一行第二个元素大小
+    printf("%d\n", sizeof(a + 1));            //  4  a没有&也没有单独放在sizeof内  所以就表示首元素地址 二维数组首元素就是第一行
+    printf("%d\n", sizeof(*(a + 1)));        //  16
+    printf("%d\n", sizeof(&a[0] + 1));      // 4
+    printf("%d\n", sizeof(*(&a[0] + 1)));   // 16
+    printf("%d\n", sizeof(*a));                   // 16
+
+    printf("%d\n", sizeof(a[3]));                 // ?  所以不会报错并没有真正计算 16
+    //  所以其实不存在 也能计算类型大小
+    // 大坑 
+    /*
+    eg : 3+5
+    表达式 :
+                    值属性 : 8
+                    类型属性 : int 
+            sizeof(3+5) - 4 可以通过类型直接得到！
+    */
+}
+
+/*
+总结：
+数组名的意义：
+1. sizeof(数组名)，这里的数组名表示整个数组，计算的是整个数组的大小。
+2. &数组名，这里的数组名表示整个数组，取出的是整个数组的地址。
+3. 除此之外所有的数组名都表示首元素的地址。
+*/
+
+/*
+笔试题1：
+int main()
+{
+    int a[5] = { 1, 2, 3, 4, 5 };
+    int* ptr = (int*)(&a + 1);
+    printf("%d,%d", *(a + 1), *(ptr - 1));
+    return 0;
+}
+答案 2 - 5
+
+笔试题2
+//由于还没学习结构体，这里告知结构体的大小是20个字节
+struct Test
+{
+ int Num;
+ char *pcName;
+ short sDate;
+ char cha[2];
+ short sBa[4];
+}*p;
+//假设p 的值为0x100000。 如下表表达式的值分别为多少？
+//已知，结构体Test类型的变量大小是20个字节
+int main()
+{
+ printf("%p\n", p + 0x1);  
+ printf("%p\n", (unsigned long)p + 0x1);
+ printf("%p\n", (unsigned int*)p + 0x1);
+ return 0;
+}
+0x100014
+0x100001
+0x100004
+
+笔试题3
+int main()
+{
+    int a[4] = { 1, 2, 3, 4 };
+    int *ptr1 = (int *)(&a + 1);
+    int *ptr2 = (int *)((int)a + 1);
+    printf( "%x,%x", ptr1[-1], *ptr2);
+    return 0;
+}
+0x00000004
+0x02000000
+
+笔试题4
+#include <stdio.h>
+int main()
+{
+    int a[3][2] = { (0, 1), (2, 3), (4, 5) };
+    int *p;
+    p = a[0];
+    printf( "%d", p[0]);
+ return 0;
+}
+注意逗号表达式！
+答案 1
+
+笔试题5
+int main()
+{
+    int a[5][5];
+    int(*p)[4];
+    p = a;
+    printf( "%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+    return 0;
+}
+答案 FFFFFFFC -4
+--->  好好看这道题 有意思的
+
+笔试题6
+int main()
+{
+    int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int *ptr1 = (int *)(&aa + 1);
+    int *ptr2 = (int *)(*(aa + 1));
+    printf( "%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+    return 0;
+}
+答案 10 5
+这道题也有点意思 p981的int*  是用来迷惑的
+
+笔试题7
+#include <stdio.h>
+int main()
+{
+ char *a[] = {"work","at","alibaba"};
+ char**pa = a;
+ pa++;
+ printf("%s\n", *pa);
+ return 0;
+}
+答案 at
+
+笔试题8
+int main()
+{
+ char *c[] = {"ENTER","NEW","POINT","FIRST"};
+ char**cp[] = {c+3,c+2,c+1,c};
+ char***cpp = cp;
+ printf("%s\n", **++cpp);
+ printf("%s\n", *--*++cpp+3);
+ printf("%s\n", *cpp[-2]+3);
+ printf("%s\n", cpp[-1][-1]+1);
+ return 0;
+}
+POINT - ER - ST - EW
+看视频解析
+有点难度
+*/
