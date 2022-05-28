7bc486cdf9d6be87ea0798a48d4ad339e5687049
WxylkxyZz <2501375882@qq.com>
2022-05-28 19:53:12 +0800

    Default

--- C语言学习代码_高级/Section_7/Primary.c
+++ C语言学习代码_高级/Section_7/Primary.c
@@ -1,7 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS	1
 #include <stdio.h>
-
 
 int main()
 {
-	return 0;
+    int a, b, c, m, t;
+    printf("请输入两个数:\n");
+    scanf("%d%d", &a, &b);
+    if (a < b)
+    {
+        t = a;
+        a = b;
+        b = t;
+    }
+    m = a * b;
+    c = a % b;
+    while (c != 0)
+    {
+        a = b;
+        b = c;
+        c = a % b;
+    }
+    printf("最大公约数是:\n%d\n", b);
+    printf("最小公倍数是:\n%d\n", m / b);
 }
